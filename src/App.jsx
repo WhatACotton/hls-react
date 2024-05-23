f83391e2fb037e6b4ed1393c023d8b3329121285
WhatACotton <whatacotton@gmail.com>
2024-05-23 18:20:36 +0900

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactHlsPlayer from 'react-hls-player';
+
+export default function App() {
+  window.Hls = "https://cdnjs.cloudflare.com/ajax/libs/hls.js/0.12.4/hls.js";
+  return (
+    <div>
+      <h1>React HLS Player</h1>
+      <ReactHlsPlayer
+        src="https://agcdn02.cdnext.stream.ne.jp/hls2/basic/data/prog_index.m3u8"
+        autoPlay={false}
+        controls={true}
+        width="100%"
+        height="auto"
+      />
+      <h1>React YouTube Player</h1>
+    </div>
+  );
+}
