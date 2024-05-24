f0f9a5397217116afe76b21817ccae639eac60b2
WhatACotton <whatacotton@gmail.com>
2024-05-24 17:45:47 +0900

    add: play pause button

--- src/player.jsx
+++ src/player.jsx
@@ -1,16 +1,28 @@
 
 import ReactPlayer from "react-player";
-export default function VideoPlayer(data) {
+import React, { useState } from 'react'
 
+export default function VideoPlayer(data) {
 console.log(data.videoSrc)
-  
+  const [playing, setPlaying] = useState(true)
   return (
+    <>
     <ReactPlayer
             url={data.videoSrc}
             className='react-player'
-            controls
+            autoPlay
+            playing={playing}
             width='100%'
             height='100%'
           />
+          <button onClick={() => 
+          {
+            setPlaying(!playing)
+            console.log(playing)
+          }
+          }>
+            {playing ? 'Pause' : 'Play'}
+          </button>
+          </>
   )
 }
